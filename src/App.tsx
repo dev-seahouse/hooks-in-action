2c5f3c815da2be065597e18ee3abaf5741f2893d
kenan <xinkenan.11@hotmail.com>
2022-10-05 18:36:05 +0800

    updated package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { FaCalendarAlt } from 'react-icons/fa';
 import './App.css';
 
 function App() {
-  return <>hello</>;
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bookings" className="btn btn-header">
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
